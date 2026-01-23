d4baa431b28f279ded7cd8dc8062722180e1dccd
bansekristian <banse.kristian@gmail.com>
2026-01-23 16:33:39 +0100

    fixes :: poging

--- src/CSharp/CodeGen.hs
+++ src/CSharp/CodeGen.hs
@@ -11,15 +11,13 @@ import qualified Data.Map as M
 
 -- The types that we generate for each datatype: our type variables for the algebra.
 -- Change these definitions instead of the function signatures to get better type errors.
-type C = Code -- class
-type M = Code -- member
-type S = Env -> (Env, Code)  -- statement
-type E = Env -> ValueOrAddress -> Code -- expression
 
--- maps var names to stack pos
+type C = Code
+type M = Code
+type S = Env -> (Env, Code)
+type E = Env -> ValueOrAddress -> Code
 type Env = M.Map Ident Int
 
--- helper for env management
 extendEnv :: Ident -> Env -> Env
 extendEnv x env = M.insert x (M.size env) env
 
@@ -29,7 +27,6 @@ lookupVar x env =
     Just i  -> i
     Nothing -> error ("use of undeclared variable: " ++ x)
 
--- main code gen alg
 codeAlgebra :: CSharpAlgebra C M S E
 codeAlgebra = CSharpAlgebra
   fClass
@@ -47,34 +44,30 @@ codeAlgebra = CSharpAlgebra
   fExprCall
 
 fClass :: ClassName -> [M] -> C
-fClass c ms = [Bsr "main", HALT] ++ concat ms
+fClass _ ms = [Bsr "main", HALT] ++ concat ms
 
 fMembDecl :: Decl -> M
-fMembDecl (Decl _ x) = []  -- member vars apart handled in real implem
+fMembDecl _ = []
 
 fMembMeth :: RetType -> Ident -> [Decl] -> S -> M
 fMembMeth retType funcName params body =
-  let -- build init env from params
-      initEnv = M.fromList [(name, i) | (Decl _ name, i) <- zip params [0..]]
+  let initEnv = M.fromList [(name, i) | (Decl _ name, i) <- zip params [0..]]
       (finalEnv, bodyCode) = body initEnv
-      -- calc number local decl
       numLocals = M.size finalEnv - length params
-      -- reserve local sapce
       linkCode = if numLocals > 0 then [LINK numLocals] else []
-      -- bye link -> return
       unlinkCode = if numLocals > 0 then [UNLINK] else []
       retCode = case retType of
         TyVoid -> [RET]
-        _      -> []  -- non void should have ret with value
+        _ -> []
   in [LABEL funcName] ++ linkCode ++ bodyCode ++ unlinkCode ++ retCode
 
 fStatDecl :: Decl -> S
 fStatDecl (Decl _ ident) env =
   let env' = extendEnv ident env
-  in (env', [LDC 0])  -- get space -> init to 0
+  in (env', [LDC 0])
 
 fStatExpr :: E -> S
-fStatExpr e env = (env, e env Value ++ [AJS (-1)])  -- pop res
+fStatExpr e env = (env, e env Value ++ [AJS (-1)])
 
 fStatIf :: E -> S -> S -> S
 fStatIf e s1 s2 env =
@@ -96,22 +89,19 @@ fStatReturn e env = (env, e env Value ++ [RET])
 
 fStatBlock :: [S] -> S
 fStatBlock statements env0 =
-  let -- process statements seq, paralell env
-      processStats [] currentEnv codes = (currentEnv, codes)
-      processStats (s:ss) currentEnv codes =
-        let (newEnv, code) = s currentEnv
-        in processStats ss newEnv (codes ++ code)
-      
-      (finalEnv, allCode) = processStats statements env0 []
-      
-      numLocals = M.size finalEnv - M.size env0
-      
-      cleanupCode = if numLocals > 0 then [AJS (-numLocals)] else []
-      
-  in (env0, allCode ++ cleanupCode)  -- ret og env
+  let -- Thread environment through statements, accumulating code
+      step (currentEnv, accCode) stmt =
+        let (newEnv, stmtCode) = stmt currentEnv
+        in (newEnv, accCode ++ stmtCode)
+      (finalEnv, allCode) = foldl step (env0, []) statements
+      -- Count new locals declared in this block
+      numNewLocals = M.size finalEnv - M.size env0
+      -- Clean up locals at end of block
+      cleanupCode = if numNewLocals > 0 then [AJS (-numNewLocals)] else []
+  in (env0, allCode ++ cleanupCode)  -- Return original env (block scope)
 
 fExprLit :: Literal -> E
-fExprLit l env va = [LDC n]
+fExprLit l _ _ = [LDC n]
   where
     n = case l of
       LitInt n -> n
@@ -121,51 +111,40 @@ fExprVar :: Ident -> E
 fExprVar x env va =
   let loc = lookupVar x env
   in case va of
-    Value   -> [LDL loc]
+    Value -> [LDL loc]
     Address -> [LDLA loc]
 
 fExprOp :: Operator -> E -> E -> E
 fExprOp OpAsg e1 e2 env va =
-  let c2 = e2 env Value     -- eval right
-      c1 = e1 env Address   -- get address of left
-      storeCode = [STA 0]   -- store val at address
+  let c2 = e2 env Value
+      c1 = e1 env Address
+      storeCode = [STA 0]
       loadCode = case va of
-        Value   -> [LDL 0]  -- if used as value load
-        Address -> []       -- if used as address dont
+        Value -> [LDL 0]
+        Address -> []
   in c2 ++ c1 ++ storeCode ++ loadCode
 
--- lazy evaluation and
-fExprOp OpAnd e1 e2 env va =
+fExprOp OpAnd e1 e2 env _ =
   let c1 = e1 env Value
       c2 = e2 env Value
       n2 = codeSize c2
   in c1 ++ [BRF (n2 + 2)] ++ c2 ++ [BRA 2, LDC 0]
 
--- lazy eval or
-fExprOp OpOr e1 e2 env va =
+fExprOp OpOr e1 e2 env _ =
   let c1 = e1 env Value
       c2 = e2 env Value
       n2 = codeSize c2
   in c1 ++ [BRT (n2 + 2)] ++ c2 ++ [BRA 2, LDC (-1)]
 
--- other eager mfs
-fExprOp op e1 e2 env va =
+fExprOp op e1 e2 env _ =
   let c1 = e1 env Value
       c2 = e2 env Value
       opCode = case op of
-        OpAdd -> ADD
-        OpSub -> SUB
-        OpMul -> MUL
-        OpDiv -> DIV
-        OpMod -> MOD
-        OpXor -> XOR
-        OpLeq -> LE
-        OpLt  -> LT
-        OpGeq -> GE
-        OpGt  -> GT
-        OpEq  -> EQ
-        OpNeq -> NE
-        _     -> error "unknown operator"
+        OpAdd -> ADD; OpSub -> SUB; OpMul -> MUL; OpDiv -> DIV
+        OpMod -> MOD; OpXor -> XOR
+        OpLeq -> LE; OpLt -> LT; OpGeq -> GE; OpGt -> GT
+        OpEq -> EQ; OpNeq -> NE
+        _ -> error "unknown operator"
   in c1 ++ c2 ++ [opCode]
 
 fExprCall :: Ident -> [E] -> E
@@ -173,18 +152,13 @@ fExprCall "print" args env _ =
   concatMap (\arg -> arg env Value ++ [TRAP 0]) args
 
 fExprCall funcName args env Value =
-  let -- eval all args and push on stack
-      argCode = concatMap (\arg -> arg env Value) args
-  in argCode ++ [Bsr funcName]
+  concatMap (\arg -> arg env Value) args ++ [Bsr funcName]
 
 fExprCall _ _ _ Address =
   error "function call cannot be used as an address"
 
--- helper for distinguishinf
-data ValueOrAddress = Value | Address
-  deriving Show
+data ValueOrAddress = Value | Address deriving Show
 
--- bool2int c:
 bool2int :: Bool -> Int
 bool2int True  = -1
 bool2int False = 0
--- src/CSharp/Parser.hs
+++ src/CSharp/Parser.hs
@@ -1,56 +1,19 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use void" #-}
 
--- This module implements a parser for
---  (a subset of) the C# programming language
-
--- This parser is split into two parts:
---  1. A *Lexer* that breaks a string into a list of tokens
---  2. A *Parser* that operates over a list of tokens
-
--- This Context-Free Grammar for (a subset of) C# is implemented here.
--- In this CFG, all nonterminals start with a capital letter.
--- The terminals [upperid, lowerid, operator, const] are special tokens,
--- all other terminals are presented verbatim.
-{-
-Class ::= Class upperid { Member∗ }
-Member ::= Decl ; | Method
-Method ::= Typevoid lowerid ( Decls? ) Block
-Decls ::= Decl | Decl , Decls
-Decl ::= Type lowerid
-Block ::= { Statdecl∗ }
-Statdecl ::= Stat | Decl ;
-Stat ::= Expr ;
-       | if ( Expr ) Stat Else?
-       | while ( Expr ) Stat
-       | return Expr ;
-       | Block
-Else ::= else Stat
-Typevoid ::= Type | void
-Type ::= int | bool
-Expr ::= Exprsimple
-       | Exprsimple operator Expr
-Exprsimple ::= const | lowerid | ( Expr )
--}
-
 module CSharp.Parser where
 
 import CSharp.AbstractSyntax
-
 import ParseLib.Derived hiding (braced, bracketed, parenthesised)
 import Control.Applicative
 import Data.Char
-import Data.Maybe
-import GHC.Read (paren)
 
----- Begin Pretty-printing functions for C# syntax ----
+---- Pretty-printing functions ----
 
--- Concrete syntax of C# types
 printType :: Type -> String
 printType = \case
   { TyInt -> "int"; TyBool -> "bool"}
 
--- Concrete syntax of C# binary operators
 printOperator :: Operator -> String
 printOperator = \case
   { OpAdd -> "+"; OpSub -> "-"; OpMul -> "*"; OpDiv -> "/"
@@ -62,7 +25,6 @@ printOperator = \case
   ; OpAsg -> "="
   }
 
--- Concrete syntax of C# keywords
 printKeyword :: Keyword -> String
 printKeyword = \case
   { KeyIf    -> "if";     KeyElse   -> "else"
@@ -72,7 +34,6 @@ printKeyword = \case
   ; KeyFor   -> "for"
   }
 
--- Concrete syntax of C# punctuation
 printPunctuation :: Punctuation -> String
 printPunctuation = \case
   { POpen     -> "("; PClose    -> ")"
@@ -81,14 +42,11 @@ printPunctuation = \case
   ; Comma     -> ","; Semicolon -> ";"
   }
 
--- Concrete syntax of C# booleans
 printBool :: Bool -> String
 printBool = \case
   {True -> "true"; False -> "false"}
 
----- End Pretty-printing functions for C# syntax ----
-
----- Begin Concrete syntax that is discarded during AST construction ----
+---- Concrete syntax ----
 
 data Keyword
   = KeyIf    | KeyElse
@@ -99,28 +57,25 @@ data Keyword
   deriving (Eq, Show, Ord, Enum, Bounded)
 
 data Punctuation
-  = POpen    | PClose      -- parentheses     ()
-  | SOpen    | SClose      -- square brackets []
-  | COpen    | CClose      -- curly braces    {}
+  = POpen    | PClose
+  | SOpen    | SClose
+  | COpen    | CClose
   | Comma    | Semicolon
   deriving (Eq, Show, Ord, Enum, Bounded)
 
----- End Concrete syntax that is discarded during AST construction ----
+----- Lexer -----
 
------ Begin Lexer -----
-
-data Token  -- What the lexer returns
+data Token
   = Punctuation Punctuation
   | Keyword     Keyword
-  | Type        Type      -- the types that variables can have
-  | Operator    Operator  -- the binary operators
-  | UpperId     Ident     -- uppercase identifiers
-  | LowerId     Ident     -- lowercase identifiers
+  | Type        Type
+  | Operator    Operator
+  | UpperId     Ident
+  | LowerId     Ident
   | BoolLit     Bool
   | IntLit      Int
   deriving (Eq, Show, Ord)
 
--- Entry point for the lexer
 lexicalScanner :: Parser Char [Token]
 lexicalScanner = lexWhiteSpace *> greedyMaybe (lexToken <* lexWhiteSpace) <* eof
 
@@ -137,15 +92,13 @@ lexToken = greedyChoice
   , Just <$> lexUpperId
   ]
 
--- Helper function that can lex values of any data type
---  that is `Bounded`, an `Enum`, and has a `print` function
-lexEnum :: (Bounded a, Enum a, Eq s) =>  (a -> [s]) -> Parser s a
+lexEnum :: (Bounded a, Enum a, Eq s) => (a -> [s]) -> Parser s a
 lexEnum print = choice $ map (\a -> a <$ token (print a)) [minBound..maxBound]
 
 lexInt :: Parser Char Int
 lexInt = read <$> greedy1 (satisfy isDigit)
 
-lexLowerId,lexUpperId :: Parser Char Token
+lexLowerId, lexUpperId :: Parser Char Token
 lexLowerId = (\x xs -> LowerId (x:xs)) <$> satisfy isLower <*> lexIdent
 lexUpperId = (\x xs -> UpperId (x:xs)) <$> satisfy isUpper <*> lexIdent
 
@@ -158,10 +111,8 @@ lexWhiteSpace = greedy (satisfy isSpace)
 lexComment :: Parser Char (Maybe Token)
 lexComment = token "//" *> greedy (satisfy (/= '\n')) *> succeed Nothing
 
--- | version of greedy which returns all non Nothing values
 greedyMaybe :: Parser s (Maybe a) -> Parser s [a]
 greedyMaybe p = getJust <$> greedy p where
-  getJust :: [Maybe a] -> [a]
   getJust [] = []
   getJust (Just a : xs) = a : getJust xs
   getJust (Nothing : xs) = getJust xs
@@ -169,9 +120,7 @@ greedyMaybe p = getJust <$> greedy p where
 greedyChoice :: [Parser s a] -> Parser s a
 greedyChoice = foldr (<<|>) empty
 
------ End Lexer -----
-
------ Begin Lexer-Parser glue -----
+----- Parser glue -----
 
 keyword :: Keyword -> Parser Token ()
 keyword k = () <$ satisfy (== Keyword k)
@@ -205,24 +154,16 @@ sLowerId = anySymbol >>= \case
   LowerId x -> pure x
   _ -> failp
 
-sOperator :: Parser Token Operator
-sOperator = anySymbol >>= \case
-  Operator x -> pure x
-  _ -> failp
+----- Parser ----
 
------ End Lexer-Parser glue -----
-
------ Begin Parser ----
-
--- Entry point to the parser
 pClass :: Parser Token Class
 pClass = Class <$ keyword KeyClass <*> sUpperId <*> braced (many pMember)
 
 pMember :: Parser Token Member
 pMember =  MemberD <$> pDeclSemi
        <|> MemberM <$> pRetType <*> sLowerId <*> methArgList <*> pBlock
-    where
-  methArgList = parenthesised (option (listOf pDecl (punctuation Comma)) [])
+  where
+    methArgList = parenthesised (option (listOf pDecl (punctuation Comma)) [])
 
 pBlock :: Parser Token Stat
 pBlock = StatBlock <$> braced (many pStatDecl)
@@ -232,21 +173,20 @@ pStatDecl =  pStat
          <|> StatDecl <$> pDeclSemi
 
 pStat :: Parser Token Stat
-pStat =  StatExpr <$> pExpr <*  sSemi
+pStat =  StatExpr <$> pExpr <* sSemi
      <|> StatIf     <$ keyword KeyIf     <*> parenthesised pExpr <*> pStat <*> optionalElse
      <|> StatWhile  <$ keyword KeyWhile  <*> parenthesised pExpr <*> pStat
-     <|> StatReturn <$ keyword KeyReturn <*> pExpr               <*  sSemi
-     <|> StatFor    <$ keyword KeyFor    <*> pForHeader          <*> pStat
+     <|> StatReturn <$ keyword KeyReturn <*> pExpr <* sSemi
+     <|> StatFor    <$ keyword KeyFor    <*> pForHeader <*> pStat
      <|> pBlock
-     where
-      optionalElse = option (keyword KeyElse *> pStat) (StatBlock [])
-      pExprDecl = ForExpr <$> pExpr <|> ForDecl <$> pDecl
-      pExprDecls = option (listOf pExprDecl (punctuation Comma)) []
-      pForHeader = parenthesised $ 
-                    (,,) 
-                      <$> pExprDecls <* sSemi
-                      <*> pExpr      <* sSemi
-                      <*> pExprDecls
+  where
+    optionalElse = option (keyword KeyElse *> pStat) (StatBlock [])
+    pExprDecl = ForExpr <$> pExpr <|> ForDecl <$> pDecl
+    pExprDecls = option (listOf pExprDecl (punctuation Comma)) []
+    pForHeader = parenthesised $ 
+      (,,) <$> pExprDecls <* sSemi
+           <*> pExpr <* sSemi
+           <*> pExprDecls
 
 pLiteral :: Parser Token Literal
 pLiteral =  LitBool <$> sBoolLit
@@ -256,15 +196,24 @@ pExprSimple :: Parser Token Expr
 pExprSimple =  ExprLit  <$> pLiteral
            <|> ExprCall <$> sLowerId <*> pExprList
            <|> ExprVar  <$> sLowerId
-           <|> parenthesised pExpr 
-           where
-  pExprList :: Parser Token [Expr]
-  pExprList = parenthesised $ option (listOf pExpr (punctuation Comma)) []
+           <|> parenthesised pExpr
+  where
+    pExprList = parenthesised $ option (listOf pExpr (punctuation Comma)) []
 
 opFrom :: [Operator] -> Parser Token Operator
 opFrom ops = anySymbol >>= \case
   Operator op | op `elem` ops -> pure op
-  _                           -> empty
+  _ -> empty
+
+-- Precedence levels (highest to lowest):
+-- 1. Multiplicative: *, /, %
+-- 2. Additive: +, -
+-- 3. Relational: <, <=, >, >=
+-- 4. Equality: ==, !=
+-- 5. XOR: ^
+-- 6. AND: &&
+-- 7. OR: ||
+-- 8. Assignment: = (right-associative)
 
 pExprMul :: Parser Token Expr
 pExprMul = chainl pExprSimple (ExprOper <$> opFrom [OpMul, OpDiv, OpMod])
@@ -287,26 +236,19 @@ pExprAnd = chainl pExprXor (ExprOper <$> opFrom [OpAnd])
 pExprOr :: Parser Token Expr
 pExprOr = chainl pExprAnd (ExprOper <$> opFrom [OpOr])
 
-asgOp :: Parser Token Operator
-asgOp = anySymbol >>= \case
-  Operator OpAsg -> pure OpAsg
-  _              -> empty
-
-pExprAssign :: Parser Token Expr
-pExprAssign =
-      tryAssign
-  <|> pExprOr
-  where
-    tryAssign = do
-      lhs <- pExprOr
-      op  <- asgOp
-      rhs <- pExprAssign
-      case lhs of
-        ExprVar _ -> pure (ExprOper op lhs rhs)
-        _         -> empty
-
+-- Assignment is right-associative and lowest precedence
 pExpr :: Parser Token Expr
-pExpr = pExprAssign
+pExpr = pExprOr >>= pExprAsg
+  where
+    pExprAsg lhs = 
+      (do op <- anySymbol >>= \case
+                  Operator OpAsg -> pure OpAsg
+                  _ -> failp
+          rhs <- pExpr  -- Right-associative: recurse to pExpr
+          case lhs of
+            ExprVar _ -> pure (ExprOper op lhs rhs)
+            _ -> failp)
+      <|> pure lhs
 
 pDecl :: Parser Token Decl
 pDecl = Decl <$> pRetType <*> sLowerId
@@ -314,17 +256,11 @@ pDecl = Decl <$> pRetType <*> sLowerId
 pDeclSemi :: Parser Token Decl
 pDeclSemi = pDecl <* sSemi
 
-pType :: Parser Token Type
-pType = sType
-
 pRetType :: Parser Token RetType
-pRetType = NV <$> pType
+pRetType = NV <$> sType
        <|> TyVoid <$ keyword KeyVoid
 
--- The `Token` equivalents to some parser combinators
 parenthesised, bracketed, braced :: Parser Token b -> Parser Token b
-parenthesised p = pack (punctuation POpen) p (punctuation PClose) --(p)
-bracketed     p = pack (punctuation SOpen) p (punctuation SClose) --[p]
-braced        p = pack (punctuation COpen) p (punctuation CClose) --{p}
-
---- End Parser ----
+parenthesised p = pack (punctuation POpen) p (punctuation PClose)
+bracketed     p = pack (punctuation SOpen) p (punctuation SClose)
+braced        p = pack (punctuation COpen) p (punctuation CClose)
